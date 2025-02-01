b538e43569c02bbe6b045d3b56b2180f9fe5799a
Marcio Mars Rodrigues <db.mars@proton.me>
2025-02-01 13:40:15 -0300

    feat(steele.sql): joins I

--- steele.sql/src/main/resources/classes-script-constraints.sql
+++ steele.sql/src/main/resources/classes-script-constraints.sql
@@ -1,6 +1,6 @@
 CREATE TABLE contacts (
-                          name VARCHAR(100) NOT NULL,
-                          phone VARCHAR(15) NOT NULL UNIQUE
+    name VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE
 );
 
 INSERT INTO contacts (name, phone)
@@ -10,4 +10,30 @@ VALUES ('billybob', '8781213455');
 INSERT INTO contacts (name, phone)
 VALUES ('billybob', '87811213455');
 
+CREATE TABLE companies (
+       name VARCHAR(255) NOT NULL,
+       address VARCHAR(255) NOT NULL,
+       CONSTRAINT name_address UNIQUE (name, address)
+);
+
+CREATE TABLE houses (
+        purchase_price INT NOT NULL,
+        sale_price INT NOT NULL,
+        CONSTRAINT sales_gt_purchase CHECK(sale_price >= purchase_price)
+);
+
+ALTER TABLE companies ADD COLUMN phone VARCHAR(15);
+ALTER TABLE companies ADD COLUMN employee_count INT NOT NULL DEFAULT 1;
+ALTER TABLE companies DROP COLUMN phone;
+RENAME TABLE companies to suppliers;
+ALTER TABLE suppliers RENAME TO companies;
+ALTER TABLE companies RENAME COLUMN name TO company_name;
+ALTER TABLE companies MODIFY company_name VARCHAR(100) DEFAULT 'unknown';
+ALTER TABLE suppliers CHANGE business biz_name VARCHAR(50);
+ALTER TABLE houses ADD CONSTRAINT positive_pprice CHECK (purchase_price >= 0);
+
+
+ALTER TABLE houses DROP CONSTRAINT positive_pprice;
+
 SELECT * FROM contacts;
+SHOW TABLES;
--- steele.sql/src/main/resources/classes-script-joins.sql
+++ steele.sql/src/main/resources/classes-script-joins.sql
@@ -4,3 +4,52 @@ USE `steele-mysql`;
 SHOW TABLES;
 
 SELECT * FROM books;
+
+CREATE TABLE customers (
+                           id INT PRIMARY KEY AUTO_INCREMENT,
+                           first_name VARCHAR(50),
+                           last_name VARCHAR(50),
+                           email VARCHAR(50)
+);
+
+CREATE TABLE orders (
+                        id INT PRIMARY KEY AUTO_INCREMENT,
+                        order_date DATE,
+                        amount DECIMAL(8,2),
+                        customer_id INT,
+                        FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
+
+INSERT INTO customers (first_name, last_name, email)
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+
+
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016-02-10', 99.99, 1),
+       ('2017-11-11', 35.50, 1),
+       ('2014-12-12', 800.67, 2),
+       ('2015-01-03', 12.50, 2),
+       ('1999-04-11', 450.25, 5);
+
+SELECT * FROM customers;
+SELECT * FROM orders;
+SELECT * FROM customers, orders;
+
+SELECT * FROM customers c
+LEFT JOIN orders o on c.id = o.customer_id
+WHERE c.first_name = 'Boy';
+
+SELECT * FROM orders
+LEFT JOIN customers c on c.id = orders.customer_id ;
+
+
+SELECT c.first_name, SUM(o.amount) soma
+FROM customers c
+    LEFT JOIN orders o on c.id = o.customer_id
+GROUP BY c.first_name
+ORDER BY soma DESC;
+
